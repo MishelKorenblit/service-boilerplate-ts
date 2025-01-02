0503a65cea10742821fadec070a3f8169ffcb603
Mishel <MK131079@greendreamteam.onmicrosoft.com>
2025-01-02 22:06:02 +0200

    Add clothing controller

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { MitigaWeatherRecommendationController } from './controllers/MitigaWeatherRecommendationController'
+import { MitigaClothingRecommendationController } from './controllers/MitigaClothesRecommendationController'
 import { MitigaWeatherRecommendationService } from './services/MitigaWeatherRecommendationService'
+import { MitigaClothingRecommendationService } from './services/MitigaClothingRecomendationService'
 import { OpenWeatherDriver } from './drivers/OpenWeatherDriver'
 import { config } from './config'
 import { HttpServer } from './HttpServer'
 
 const openWeatherDriver = new OpenWeatherDriver(config.get('openWeatherMapApiKey'))
 const mitigaTravelService = new MitigaWeatherRecommendationService(openWeatherDriver)
+const mitigaClothingService = new MitigaClothingRecommendationService(openWeatherDriver)
 const mitigaTravelController = new MitigaWeatherRecommendationController(mitigaTravelService)
+const mitigaClothingController = new MitigaClothingRecommendationController(mitigaClothingService)
 
 const port = config.get('port')
-const httpServer = new HttpServer(port, mitigaTravelController)
+const httpServer = new HttpServer(port, mitigaTravelController, mitigaClothingController)
 httpServer.start().catch((error) => {
   console.error(error)
   process.exit(1)
