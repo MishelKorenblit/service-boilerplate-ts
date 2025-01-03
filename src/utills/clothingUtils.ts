cee0fca13c31c7ab11917cca763146f13741b70d
Mishel <MK131079@greendreamteam.onmicrosoft.com>
2025-01-03 13:27:01 +0200

    Remove function and add types

--- src/utills/clothingUtils.ts
+++ src/utills/clothingUtils.ts
@@ -1,4 +1,5 @@
-import type { ClothesOptions, PartOfTheDay } from '../travel-service-dto-s';
+export type ClothesOptions = "No recommendation available why aren't you sleeping?" | 'Raincoat' | 'Umbrella' | 'Sunglasses' | 'T-shirt' | 'Shorts' | 'Light jacket' | 'Long pants' | 'Light sweater' | 'Heavy coat' | 'Gloves';
+export type PartOfTheDay = 'morn' | 'day' | 'eve' | 'night';
 
 export const partOfTheDay: { [key: string]: PartOfTheDay } = {
     morning: 'morn',
@@ -14,22 +15,3 @@ export const getPartOfTheDay = (dateTime: Date): PartOfTheDay => {
     if (hour >= 18 && hour < 24) return partOfTheDay.evening
     return partOfTheDay.night
 }
-
-export const recommendedClothing = (
-    temperature: number,
-    timeOfDay: PartOfTheDay,
-    weatherConditions: string
-): ClothesOptions[] => {
-    if (weatherConditions === 'Rainy') return ['Raincoat', 'Umbrella'];
-    if (temperature > 18 && timeOfDay !== partOfTheDay.night && weatherConditions === 'Sunny') return ['T-shirt', 'Shorts'];
-    if (timeOfDay == partOfTheDay.morning || timeOfDay == partOfTheDay.afternoon) {
-        if (weatherConditions === 'Sunny') return ['Sunglasses'];
-    }
-    if (temperature >= 10 && temperature <= 18) {
-        if (timeOfDay === partOfTheDay.morning || timeOfDay === partOfTheDay.afternoon) return ['Light jacket', 'Long pants'];
-        if (timeOfDay === partOfTheDay.evening) return ['Light sweater', 'Long pants'];
-    }
-    if (temperature < 10) return ['Heavy coat', 'Gloves'];
-
-    return [`No recommendation available why aren't you sleeping?`];
-};
