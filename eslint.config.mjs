99ad678a6c335a68ade2fe0facce90c05aa05d13
Mishel <MK131079@greendreamteam.onmicrosoft.com>
2025-01-03 02:19:36 +0200

    Add eslint spell

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import  cspellESLintPluginRecommended from '@cspell/eslint-plugin/recommended';
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -10,4 +11,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  cspellESLintPluginRecommended
 ];
